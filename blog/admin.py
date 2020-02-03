81cfbcb23a89f8cad9916f989660e66de88f4891
chandram10 <chandramatthewr@gmail.com>
2020-02-02 20:40:19 -0500

    Modified templates to display posts from database.

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Post
 
+
+admin.site.site_header = "OBG Adminstration"
+admin.site.site_title = "OBG Administration"
+
 admin.site.register(Post)
 
 
